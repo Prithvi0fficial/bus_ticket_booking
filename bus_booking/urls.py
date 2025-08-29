922d576a452d75246136a2cbf76a165277579cf6
Prithvi0fficial <312walkerofficial.com@gmail.com>
2025-08-29 20:42:50 +0530

    Update urls.py

--- bus_booking/urls.py
+++ bus_booking/urls.py
@@ -16,16 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),# User authentication
-    path('', include('booking.urls')),  # Include the booking app URLs
-]+ debug_toolbar_urls()
-
+    path('users/', include('users.urls')),  # User authentication
+    path('', include('booking.urls')),      # Booking app URLs
+]
 
 if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
