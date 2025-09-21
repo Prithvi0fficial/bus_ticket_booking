3e7b6e2df7315d425d2e832ec7168884718e3ed8
Prithvi0fficial <312walkerofficial.com@gmail.com>
2025-09-22 00:03:48 +0530

    Update signals.py

--- users/signals.py
+++ users/signals.py
@@ -24,12 +24,3 @@ def ensure_profile_exists(sender, request, user, **kwargs):
         Profile.objects.get_or_create(user=user)
 
 
-# DASHBOARD
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
